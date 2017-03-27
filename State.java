f2b691dfab9d466ff4d774fc7bb019ab1a49e22e
tshjones <tshjones@yahoo.com>
2017-03-27 12:36:31 -0500

    Update State.java
    added print method

--- State.java
+++ State.java
@@ -37,4 +37,17 @@ public class State
     {
         return curState;
     }
+    // Tod - made a simple print state method. Haven't tested it, but it should come in handy when we need to debug, can
+    // make more pretty later
+    public void printCurrentState()
+    {
+        for (int i = 0; i < curState.length; i++)
+        {
+            for (for j = 0; j < curstate[i].length; j++)
+            {
+                System.out.print(curstate[i][j] + " ");
+            }
+        System.out.print("\n");
+        }
+    
 }
