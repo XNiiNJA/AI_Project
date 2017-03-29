2f4a3eb27a4dcf06a7fd675e828ff2effa8d528a
tshjones <tshjones@yahoo.com>
2017-03-29 12:45:00 -0500

    Update StateManager.java

--- src/PuzzleSolver/src/puzzlesolver/StateManager.java
+++ src/PuzzleSolver/src/puzzlesolver/StateManager.java
@@ -26,7 +26,7 @@ public class StateManager
             for(int j = 1; j < width; j++)
             {
                 //setGoal[i][i + j] =i + j;  your math is wrong - Tod
-                setGoal[i][(i * width) + j] = (i * width) + j;
+                setGoal[i][j] = (i * width) + j;
             }
         goal = new State(setGoal);
     }
